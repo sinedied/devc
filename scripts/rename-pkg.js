7d3e4fc122227c6b4d7050eaed8ebe6a927ed6c1
sinedied <noda@free.fr>
2022-03-07 15:23:14 +0100

    build: add script to rename package

---
+++ scripts/rename-pkg.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+// ***************************************************************************
+// Changes the name of a package.
+// Usage: rename-pkg.js <new-name>
+// ***************************************************************************
+
+const process = require('process');
+const fs = require('fs');
+
+const args = process.argv.slice(2);
+if (args.length !== 1 || !args[0]) {
+  console.error('Usage: rename-pkg <new-name>');
+  process.exitCode = -1;
+}
+
+const pkg = JSON.parse(fs.readFileSync('package.json'));
+pkg.name = args[0];
+fs.writeFileSync('package.json', JSON.stringify(pkg, null, 2) + '\n');
+console.info(`Renamed package to ${pkg.name}`);
