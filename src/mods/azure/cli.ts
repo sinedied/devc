4f775a46c06be93abb8073a2c3ea9540c928326e
sinedied <noda@free.fr>
2022-03-09 18:14:00 +0100

    fix: azure cli install script

--- src/mods/azure/cli.ts
+++ src/mods/azure/cli.ts
@@ -3,5 +3,5 @@ import { Mod } from '../../mod.js';
 export const azureCli: Mod = {
   containerSetup:
     '# Install Azure CLI\n' +
-    'RUN bash <(curl -sfL "https://raw.githubusercontent.com/microsoft/vscode-dev-containers/main/script-library/azcli-debian.sh") && apt-get clean -y && rm -rf /var/lib/apt/lists/*'
+    'RUN curl -sfL "https://raw.githubusercontent.com/microsoft/vscode-dev-containers/main/script-library/azcli-debian.sh" | bash && apt-get clean -y && rm -rf /var/lib/apt/lists/*'
 };
